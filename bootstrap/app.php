a5cba6e51bef9959e02bde5221de27f0519211d9
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2025-11-04 15:14:39 +0100

    create and register middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,13 +6,25 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+    web: null,
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+    $middleware->api([
+        \Illuminate\Http\Middleware\HandleCors::class,
+        'throttle:api',
+        \Illuminate\Routing\Middleware\SubstituteBindings::class,
+    ]);
+
+    $middleware->alias([
+        'auth' => \App\Http\Middleware\Authenticate::class,
+        'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+        'can' => \Illuminate\Auth\Middleware\Authorize::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'admin' => \App\Http\Middleware\AdminAccess::class,
+    ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
